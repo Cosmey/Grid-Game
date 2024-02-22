b554af15b7f9199f6b2c2600fef40d2d17143ddf
Finnley Stoakley <fjsstoakley@gmail.com>
2024-02-22 12:40:12 -0500

    Added Grid System

---
+++ Grid Game/Assets/GridTile.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTile : MonoBehaviour
+{
+    public GridManager gridManager;
+
+    public Vector2Int gridCoords;
+
+    public SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = transform.Find("Square").GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = false;
+    }
+    private void OnMouseOver()
+    {
+        spriteRenderer.enabled = true;
+        gridManager.OnTileHoverEnter(this);
+    }
+
+    private void OnMouseExit()
+    {
+        spriteRenderer.enabled=false;
+        gridManager.OnTileHoverExit(this);
+    }
+}
